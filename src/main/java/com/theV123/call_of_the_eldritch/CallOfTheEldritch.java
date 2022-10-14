f99163947f6b4c0e9b698eed7190fbff7889027c
Vishal Kamalakrishnan <vishkamalk@gmail.com>
2022-10-14 14:37:22 -0400

    Initial commit

---
+++ src/main/java/com/theV123/call_of_the_eldritch/CallOfTheEldritch.java
@@ -0,0 +1,36 @@
+package com.theV123.call_of_the_eldritch;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(CallOfTheEldritch.MOD_ID)
+public class CallOfTheEldritch
+{
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final String MOD_ID = "call_of_the_eldritch";
+
+    public CallOfTheEldritch()
+    {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        // Register the enqueueIMC method for modloading
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+
+}
