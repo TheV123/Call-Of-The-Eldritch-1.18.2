a733048b77114a18333a8552f36c65270b33c408
Vishal Kamalakrishnan <vishkamalk@gmail.com>
2022-10-23 01:49:39 -0400

    change method name

--- src/main/java/com/theV123/call_of_the_eldritch/block/ModBlocks.java
+++ src/main/java/com/theV123/call_of_the_eldritch/block/ModBlocks.java
@@ -11,13 +11,20 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.util.function.Supplier;
+
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, CallOfTheEldritch.MOD_ID);
 
+    private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block,
+                                                                    CreativeModeTab tab){
+        RegistryObject<T> returned = BLOCKS.register(name, block);
+        registerBlockItem(name, returned, tab);
+        return returned;
+    }
 
-
-    private static <T extends Block>RegistryObject<Item> registryBlockItem(String name, RegistryObject<T> block,
+    private static <T extends Block>RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block,
                                                                            CreativeModeTab tab){
         return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
                 new Item.Properties().tab(tab)));
